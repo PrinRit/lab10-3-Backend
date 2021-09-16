a25ea2d65415a4bdb4e90ea390c56e084f1c4a1f
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-09-16 19:05:28 +0700

    3.1 Add full Bid

--- src/main/java/se331/lab/rest/config/InitApp.java
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -22,13 +22,22 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     @Transactional
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
 
-        Bid bid1,bid2,bid3,bid4,bid5,bid6;
+        Bid bid1,bid2,bid3,bid4,bid5,bid6,bid7,bid8,bid9,bid10,bid11,bid12,bid13,bid14,bid15;
         bid1 = bidRepository.save(Bid.builder().amount(5).datetime("2021-01-01").build());
         bid2 = bidRepository.save(Bid.builder().amount(10).datetime("2021-02-02").build());
         bid3 = bidRepository.save(Bid.builder().amount(15).datetime("2021-03-03").build());
         bid4 = bidRepository.save(Bid.builder().amount(20).datetime("2021-04-04").build());
         bid5 = bidRepository.save(Bid.builder().amount(25).datetime("2021-05-05").build());
         bid6 = bidRepository.save(Bid.builder().amount(30).datetime("2021-06-06").build());
+        bid7 = bidRepository.save(Bid.builder().amount(35).datetime("2021-07-07").build());
+        bid8 = bidRepository.save(Bid.builder().amount(40).datetime("2021-08-08").build());
+        bid9 = bidRepository.save(Bid.builder().amount(45).datetime("2021-09-09").build());
+        bid10 = bidRepository.save(Bid.builder().amount(50).datetime("2021-10-10").build());
+        bid11 = bidRepository.save(Bid.builder().amount(55).datetime("2021-11-11").build());
+        bid12 = bidRepository.save(Bid.builder().amount(60).datetime("2021-12-12").build());
+        bid13 = bidRepository.save(Bid.builder().amount(65).datetime("2021-12-13").build());
+        bid14 = bidRepository.save(Bid.builder().amount(70).datetime("2021-12-14").build());
+        bid15 = bidRepository.save(Bid.builder().amount(75).datetime("2021-12-15").build());
 
         AuctionItem Auc1,Auc2,Auc3,Auc4,Auc5;
 
@@ -68,49 +77,49 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .description("A bigest electricity shop")
                 .type("Silver")
                 .build());
-        bid2.setItem(Auc3);
-        Auc3.getBids().add(bid2);
-        Auc3.setSuccessfulBid(bid2);
+        bid7.setItem(Auc3);
+        Auc3.getBids().add(bid7);
+        Auc3.setSuccessfulBid(bid7);
 
-        bid3.setItem(Auc3);
-        Auc3.getBids().add(bid3);
-        Auc3.setSuccessfulBid(bid3);
+        bid8.setItem(Auc3);
+        Auc3.getBids().add(bid8);
+        Auc3.setSuccessfulBid(bid8);
 
-        bid4.setItem(Auc3);
-        Auc3.getBids().add(bid4);
-        Auc3.setSuccessfulBid(bid4);
+        bid9.setItem(Auc3);
+        Auc3.getBids().add(bid9);
+        Auc3.setSuccessfulBid(bid9);
 
         Auc4 = auctionItemRepository.save(AuctionItem.builder()
                 .description("A King of Mumbai")
                 .type("Money")
                 .build());
-        bid1.setItem(Auc4);
-        Auc4.getBids().add(bid1);
-        Auc4.setSuccessfulBid(bid1);
+        bid10.setItem(Auc4);
+        Auc4.getBids().add(bid10);
+        Auc4.setSuccessfulBid(bid10);
 
-        bid5.setItem(Auc4);
-        Auc4.getBids().add(bid5);
-        Auc4.setSuccessfulBid(bid5);
+        bid11.setItem(Auc4);
+        Auc4.getBids().add(bid11);
+        Auc4.setSuccessfulBid(bid11);
 
-        bid6.setItem(Auc4);
-        Auc4.getBids().add(bid6);
-        Auc4.setSuccessfulBid(bid6);
+        bid12.setItem(Auc4);
+        Auc4.getBids().add(bid12);
+        Auc4.setSuccessfulBid(bid12);
 
         Auc5 = auctionItemRepository.save(AuctionItem.builder()
                 .description("Operators")
                 .type("Gold")
                 .build());
-        bid3.setItem(Auc5);
-        Auc5.getBids().add(bid3);
-        Auc5.setSuccessfulBid(bid3);
+        bid13.setItem(Auc5);
+        Auc5.getBids().add(bid13);
+        Auc5.setSuccessfulBid(bid13);
 
-        bid5.setItem(Auc5);
-        Auc5.getBids().add(bid5);
-        Auc5.setSuccessfulBid(bid5);
+        bid14.setItem(Auc5);
+        Auc5.getBids().add(bid14);
+        Auc5.setSuccessfulBid(bid14);
 
-        bid6.setItem(Auc5);
-        Auc5.getBids().add(bid6);
-        Auc5.setSuccessfulBid(bid6);
+        bid15.setItem(Auc5);
+        Auc5.getBids().add(bid15);
+        Auc5.setSuccessfulBid(bid15);
 
     }
 }
