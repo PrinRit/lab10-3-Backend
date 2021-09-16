9674598d90b87ec751da8082a1759821bd324ec7
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-09-16 15:24:05 +0700

    init new backend project

---
+++ src/main/java/se331/lab/rest/entity/AuctionItem.java
@@ -0,0 +1,21 @@
+package se331.lab.rest.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuctionItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
+    String description;
+    String type;
+}
